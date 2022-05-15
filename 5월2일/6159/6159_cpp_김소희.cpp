25e9baa9716f8d6c80837e79bb6659ccec73d4ae
ssohee <prinso@naver.com>
2022-05-15 23:19:09 +0900

    week9 retry

--- 5월2일/6159/6159_cpp_김소희.cpp
+++ 5월2일/6159/6159_cpp_김소희.cpp
@@ -1,31 +1,32 @@
 #include "bits/stdc++.h"
 
 using namespace std;
-//   6159, 18126, 11950, 24463, 17291, 24955, 20159
+//   24463, 17291, 24955, 20159
+//18126,11950,
 int n, s;
-vector<int> v;
-
-
-
+const int MAX = 20001;
+int cow[MAX];
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cin >> n >> s;
-	for (int i = 0; i < n; i++)
-	{
-		int l;
-		cin >> l;
-		v.push_back(l);
-	}
-	sort(v.begin(), v.end());
 	int cnt = 0;
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = i + 1; j < n; j++)
-		{
-			if (v.at(i) + v.at(j) <= s)cnt++;
+		cin >> cow[i];
+
+	}
+	sort(cow, cow+n);
+	int st = 0, e = n - 1;
+	while (st < e) {
+		if (cow[st] + cow[e] <= s)
+			cnt = cnt + e - st;
+		else {
+			e -= 1;
+			st -= 1;
 		}
+		st += 1;
 	}
 	cout << cnt;
 }
